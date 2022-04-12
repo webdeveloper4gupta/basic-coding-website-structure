45693b775297e275d715ef61f429e273f02657c8
Sukritan Gupta <20051871@kiit.ac.in>
2022-04-12 23:40:18 +0530

    after creation of index.js and about.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let a = document.getElementById("more-option-button");
+
+
+a.addEventListener('click', () => {
+    let b = document.getElementById('row3');
+    if (!(b.style.display === "none")) {
+        b.style.display = "none";
+        $("#more-option-button").text("More option").css({ 'background': 'blue', "width": "110px" });
+
+    }
+    else if (!(b.style.display === "flex")) {
+        b.style.display = "flex";
+        $("#more-option-button").text("less").css({ 'background': 'blue', "width": "110px" });
+
+
+    }
+});
